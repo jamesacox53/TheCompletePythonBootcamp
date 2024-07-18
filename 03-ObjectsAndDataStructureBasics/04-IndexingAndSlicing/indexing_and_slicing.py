a69fac4695cf1a7ee215e84e15522b7e33f497c9
jamesacox53 <jamesacox53@gmail.com>
2024-07-18 15:55:01 +0100

    Completed Indexing and Slicing with Strings

---
+++ 03-ObjectsAndDataStructureBasics/04-IndexingAndSlicing/indexing_and_slicing.py
@@ -0,0 +1,23 @@
+my_string = 'Hello World'
+
+print(my_string[0])
+
+print(my_string[8])
+
+print(my_string[-2])
+
+my_string = 'abcdefghijk'
+
+print(my_string[2:])
+
+print(my_string[0:3])
+
+print(my_string[3:6])
+
+print(my_string[::])
+
+print(my_string[::2])
+
+print(my_string[2:7:2])
+
+print(my_string[::-1])

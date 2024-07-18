0a72e2c5d1bc6055cec60a3496dc86bd6ba8ba87
jamesacox53 <jamesacox53@gmail.com>
2024-07-18 18:01:03 +0100

    Completed String Properties and Methods

---
+++ 03-ObjectsAndDataStructureBasics/05-StringPropertiesAndMethods/string_properties_and_methods.py
@@ -0,0 +1,20 @@
+name = "Sam"
+print(name)
+
+last_letters = name[1:]
+name = 'P' + last_letters
+print(name)
+
+x = "Hello World"
+x = x + ", it's beautiful outside!"
+print(x)
+
+letter = 'z'
+sleep = letter * 10
+print(sleep)
+
+y = 'Hello World'
+print(y.upper())
+print(y.lower())
+print(y.split())
+print(y.split('l'))

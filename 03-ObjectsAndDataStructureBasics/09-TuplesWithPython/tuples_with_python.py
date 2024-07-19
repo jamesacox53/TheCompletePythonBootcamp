0a075723405e4a251fc25c94847027acc1d952c2
jamesacox53 <jamesacox53@gmail.com>
2024-07-19 14:11:03 +0100

    Completed Tuples with Python

---
+++ 03-ObjectsAndDataStructureBasics/09-TuplesWithPython/tuples_with_python.py
@@ -0,0 +1,17 @@
+t = (1, 2, 3)
+my_list = [1, 2, 3]
+
+print(type(t))
+print(type(my_list))
+
+print(len(t))
+
+t2 = ('one', 2, 3.0)
+print(t2)
+
+print(t2[0])
+
+t3 = ('a', 'a', 'b')
+print(t3.count('a'))
+print(t3.index('a'))
+print(t3.index('b'))

5e13382e255ac070b3b8a1021e2161479e9f3f6a
jamesacox53 <jamesacox53@gmail.com>
2024-07-22 13:04:19 +0100

    Completed If Elif and Else Statements in Python

---
+++ 05-PythonStatements/01-IfElifElseStatements/if_elif_else_statements.py
@@ -0,0 +1,39 @@
+if True:
+    print("It's True!")
+
+if (3 > 2):
+    print("It's True!")
+
+hungry = True
+
+if (hungry):
+    print("Feed Me!")
+
+hungry = False
+
+if (hungry):
+    print("Feed Me!")
+else:
+    print("I'm not hungry")
+
+location = 'Bank'
+
+if (location == 'Auto Shop'):
+    print("Cars are cool!")
+elif (location == 'Bank'):
+    print("Money is cool!")
+elif (location == 'Store'):
+    print("Stuff is cool!")
+else:
+    print("I don't know much.")
+
+name = 'Sammy'
+
+if (name == 'Frankie'):
+    print("Hello Frankie!")
+
+elif (name == 'Sammy'):
+    print("Hello Sammy!")
+
+else:
+    print("What is your name?")

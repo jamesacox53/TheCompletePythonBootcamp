b0946dbf2fb72291eed86e757533b23b5e081cf3
jamesacox53 <jamesacox53@gmail.com>
2024-07-22 12:48:03 +0100

    Completed Chaining Comparison Operators in Python with Logical Operators

---
+++ 04-PythonComparisonOperators/02-LogicalOperators/logical_operators.py
@@ -0,0 +1,21 @@
+print(1 < 2 < 3)
+
+print(1 < 2 > 3)
+
+print(1 < 2 and 2 < 3)
+
+print(1 < 2 and 2 > 3)
+
+print('h' == 'h' and 2 == 2)
+
+print((1 == 1) or (2 == 2))
+
+print((100 == 1) or (2 == 2))
+
+print((100 == 1) or (200 == 2))
+
+print(not(1 == 1))
+
+print(400 > 5000)
+
+print(not(400 > 5000))

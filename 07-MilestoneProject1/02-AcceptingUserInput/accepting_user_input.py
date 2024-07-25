ae0e06b984fffec347e301317bceab9bc90e22c8
jamesacox53 <jamesacox53@gmail.com>
2024-07-25 11:45:49 +0100

    Completed Accepting User Input

---
+++ 07-MilestoneProject1/02-AcceptingUserInput/accepting_user_input.py
@@ -0,0 +1,15 @@
+result = input('Please enter a value: ')
+print(result)
+
+val = '2'
+
+val_int = int(val)
+print(val_int)
+print(type(val_int))
+
+val2 = '2.3'
+val2_flt = float(val2)
+print(val2_flt)
+print(type(val2_flt))
+
+result2 = int(input('Please enter a value: '))

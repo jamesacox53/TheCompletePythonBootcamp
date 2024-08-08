7002ed434460f8043453a8a4fe4f6daf27eb9dbb
jamesacox53 <jamesacox53@gmail.com>
2024-08-08 17:47:27 +0100

    Completed Advanced Numbers

---
+++ 20-AdvancedObjectsAndDataStructures/01-AdvancedNumbers/advanced_numbers.py
@@ -0,0 +1,25 @@
+print(hex(12))
+
+print(hex(512))
+
+print(bin(1234))
+
+print(bin(128))
+
+print(bin(512))
+
+print(pow(2, 4))
+
+print(pow(2, 4, 3))
+
+print(abs(2))
+
+print(abs(-2))
+
+print(round(3.1))
+
+print(round(3))
+
+print(round(3.9))
+
+print(round(3.14159, 2))
